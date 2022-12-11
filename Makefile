d546ff2d1a6cf9a2bfb3626bd7551f80fcf81050
MTT555 <mateusz.tyl.stud@pw.edu.pl>
2022-12-11 19:35:02 +0100

    Wersja ostateczna

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	gcc -Wall --pedantic src/*.c -o bin/gauss
+	gcc -Wall --pedantic -lm src/*.c -o bin/gauss
 
 test: all
 	bin/gauss dane/A dane/b
@@ -11,4 +11,6 @@ test4: all
 	bin/gauss dane/A4 dane/b4
 test5: all
 	bin/gauss dane/A5 dane/b5
+zeraNaPrzekatnej: all
+	bin/gauss dane/zeraNaPrzekatnejA dane/zeraNaPrzekatnejb
 	
--- src/gauss.c
+++ src/gauss.c
@@ -1,6 +1,6 @@
 #include "gauss.h"
 #include <stdio.h>
-
+#include <math.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -8,10 +8,27 @@
 int eliminate(Matrix *mat, Matrix *b){
     
 	// zeruje kolejne kolumny pod przekatna
-	// nie uwzgledniam na razie przypadku gdy w przekatnej bedzie 0
-	// to bedzie uwzglednione w innej wersji programu
-	for(int kolumny = 0; kolumny < mat->c; kolumny++){
-		
+	// w tej wersji programu uwzgledniam przypadek kiedy na przekatnej moze byc 0
+	// wtedy program dokonuje zamiany kolejnosci wierszy
+	int kolumny = 0;
+	for(kolumny = 0; kolumny < mat->c; kolumny++){
+		int kolumnyMAX = kolumny;
+		for (int wiersze = kolumny+1; wiersze < mat->r; wiersze++) 
+			if(fabs(mat->data[wiersze][kolumny]) > fabs(mat->data[kolumnyMAX][kolumny])) kolumnyMAX = wiersze;
+	if (kolumnyMAX != kolumny){
+	// zamieniamy kolejnosc wierszy
+	double *tymczasowy = mat->data[kolumny];
+	mat->data[kolumny] = mat->data[kolumnyMAX];
+	mat->data[kolumnyMAX] = tymczasowy;
+	double *bTymczasowy = b->data[kolumny];
+	b->data[kolumny] = b->data[kolumnyMAX];
+	b->data[kolumnyMAX] = bTymczasowy;
+
+	}
+	}
+	printf("Stan po zamianie kolejnosci wierszy: \n");
+		printToScreen(mat);
+		for(int kolumny = 0; kolumny < mat->c; kolumny++){
 		for(int wiersze = kolumny+1; wiersze < mat->r; wiersze++){
 			if(mat->data[kolumny][kolumny] == 0) return 1; // macierz osobliwa
 			double mnoznik = mat->data[wiersze][kolumny]/mat->data[kolumny][kolumny];
@@ -22,7 +39,7 @@ int eliminate(Matrix *mat, Matrix *b){
 			}
 			b->data[wiersze][0] -= mnoznik * b->data[kolumny][0];
 		}
-	}
+		}
 		printf("Stan po schodkowaniu: \n");
 		printToScreen(mat);
 		return 0;

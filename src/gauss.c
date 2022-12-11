e74c8ad1be84a7cfde6233fdb5b761dec2925904
MTT555 <mateusz.tyl.stud@pw.edu.pl>
2022-12-11 16:22:09 +0100

    Dodanie modulu gauss.c

--- src/gauss.c
+++ src/gauss.c
@@ -5,10 +5,24 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
+    
+	// zeruje kolejne kolumny pod przekatna
+	// nie uwzgledniam na razie przypadku gdy w przekatnej bedzie 0
+	// to bedzie uwzglednione w innej wersji programu
+	for(int kolumny = 0; kolumny < mat->c; kolumny++){
+		
+		for(int wiersze = kolumny+1; wiersze < mat->r; wiersze++){
+			if(mat->data[kolumny][kolumny] == 0) return 1; // macierz osobliwa
+			double mnoznik = mat->data[wiersze][kolumny]/mat->data[kolumny][kolumny];
+			// zerujemy kolumne ponizej przekatnej
+			for(int i = kolumny; i < mat->c; i++)
+			{
+				mat->data[wiersze][i] -= mnoznik * mat->data[kolumny][i];
+			}
+			b->data[wiersze][0] -= mnoznik * b->data[kolumny][0];
+		}
+	}
+		printToScreen(mat);
 		return 0;
 }
 
